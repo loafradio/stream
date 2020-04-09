9e1a579912fe4bf13edf96a662d4698b2e72ba8d
Bryce Thuilot <bthuilot@gmail.com>
2020-04-08 21:27:21 -0400

    Update audio stream logic

--- main.js
+++ main.js
@@ -3,47 +3,53 @@ let playing = false;
 PAUSE_TEXT = "<i class=\"fa fa-pause\" aria-hidden=\"true\"></i> Pause"
 PLAY_TEXT = "<i class=\"fa fa-play\" aria-hidden=\"true\"></i> Play"
 
-    const audio = new Audio('https://loaf.out.airtime.pro/loaf_a');
 
-    let setAudioLevel = (audioLevel) => {
+class Stream {
+
+    constructor(audio) {
+        this.audio = audio;
+        this.started = false
+        this.pauseAudio()
+    }
+
+    setAudioLevel(audioLevel) {
         if (audioLevel > 1 || audioLevel < 0) {
             return;
         }
-        audio.volume = audioLevel;
+        this.audio.volume = audioLevel;
     };
 
 
-    let playAudio = () => {
-        playing = true;
-        setAudioLevel(100);
+    playAudio() {
+        if (!this.started) {
+            this.started = true;
+            this.audio.play()
+        }
+        this.playing = true;
+        this.setAudioLevel(1);
         document.getElementById('pause-button').innerHTML = PAUSE_TEXT;
     };
 
-    let pauseAudio = () => {
-        playing = false;
-        setAudioLevel(0);
+    pauseAudio(){
+        this.playing = false;
+        this.setAudioLevel(0);
         document.getElementById('pause-button').innerHTML = PLAY_TEXT;
     };
 
-    let toggleAudio = () => {
-        if (playing) {
-            pauseAudio();
+   toggleAudio() {
+        if (this.playing) {
+            this.pauseAudio();
         } else {
-            playAudio();
+            this.playAudio();
         }
     };
+}
+var s;
 
-
-    jQuery('#pause-button').on('click', () => {
-        toggleAudio();
-    });
-
-    jQuery(document).on('keydown', event => {
-        if (event.keyCode === 32) {
-            toggleAudio();
-        }
-    });
 jQuery(document).ready(function() {
-    audio.play();
-    playAudio();
+    var audio = document.getElementById('music');
+    s = new Stream(audio);
+    jQuery('#pause-button').click(() => {
+        s.toggleAudio();
+    });
 })
